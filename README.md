905649618cedae067087742d3f5e83ceb1024015
Pavlo Ivanichenko <pasha.proton@gmail.com>
2023-11-19 23:59:08 -0800

    initial commit with code

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # minimal-select
-Minimal JavaScript select with no dependencies
+
+## Minimal web component select with no dependencies
+
+To use the library just add styles and JavaScript
+```
+<link rel="stylesheet" href="...minimal-select.min.css">
+<script src="...minimal-select.min.js"></script>
+```
+
+Then you can use select
+```
+<minimal-select name="selected-value" value="3" placeholder="Search" aria-placeholder="Search">
+  <option value="1">Option 1</option>
+  <option value="2">Option 2</option>
+  <option value="3">Option 3</option>
+  <option value="4">Option 4</option>
+  <option value="5">Option 5</option>
+  <option value="6">Option 6</option>
+  <option value="7">Option 7</option>
+  <option value="8">Option 8</option>
+  <option value="9">Option 9</option>
+  <option value="10">Option 10</option>
+</minimal-select>
+```
---
+++ src/index.js
@@ -0,0 +1,108 @@
+class MinimalSelect extends HTMLElement {
+  #options = [];
+  #valueInput;
+  #searchInput;
+  #optionStyleDisplay;
+
+  constructor() {
+    super()
+  }
+
+  #resetOptions() {
+    this.#options.forEach(option => {
+      option.style.display = this.#optionStyleDisplay;
+    });
+  }
+
+  #showOptions() {
+    this.classList.add("active");
+  }
+
+  #filterOptions(e) {
+    const value = e.target.value.toLowerCase();
+    this.#options.forEach(option => {
+      if (option.innerHTML.toLowerCase().includes(value)) {
+        option.style.display = this.#optionStyleDisplay;
+      } else {
+        option.style.display = "none";
+      }
+    });
+  }
+
+  #createValueInput() {
+    this.#valueInput = document.createElement("input");
+    this.#valueInput.setAttribute("type", "hidden");
+    this.#valueInput.setAttribute("name", this.getAttribute("name"));
+    this.#valueInput.setAttribute("value", this.getAttribute("value"));
+    this.appendChild(this.#valueInput);
+    this.removeAttribute("value");
+  }
+
+  #createSearchInput() {
+    this.#searchInput = document.createElement("input");
+    this.#searchInput.setAttribute("type", "text");
+    this.#searchInput.setAttribute("placeholder", this.getAttribute("placeholder"));
+    this.#searchInput.addEventListener("click", this.#showOptions.bind(this));
+    this.#searchInput.addEventListener("focus", this.#showOptions.bind(this));
+    this.#searchInput.addEventListener("input", this.#filterOptions.bind(this));
+    this.appendChild(this.#searchInput);
+  }
+
+  #createOptions() {
+    const optionsWrapp = document.createElement("div");
+
+    for (let i = 0; i < this.childNodes.length; i++) {
+      if (this.childNodes[i].tagName === "OPTION") {
+        const option = document.createElement("div");
+        option.innerHTML = this.childNodes[i].innerHTML;
+        option.setAttribute("data-value", this.childNodes[i].getAttribute("value"));
+        option.addEventListener("click", e => {
+          this.#valueInput.value = e.target.dataset.value;
+          this.#searchInput.value = e.target.innerHTML;
+
+          const changeEvent = new CustomEvent("onchange", {
+            detail: {
+              value: e.target.dataset.value
+            }
+          });
+          this.dispatchEvent(changeEvent);
+
+          window.setTimeout(() => {
+            this.#resetOptions();
+          }, 200);
+        });
+        optionsWrapp.appendChild(option);
+        this.#options.push(option);
+        this.#optionStyleDisplay = option.style.display;
+
+        if (this.childNodes[i].getAttribute("value") === this.#valueInput.value) {
+          this.#searchInput.value = this.childNodes[i].innerHTML;
+        }
+
+        this.childNodes[i].remove();
+      }
+    }
+
+    this.appendChild(optionsWrapp);
+  }
+
+  connectedCallback() {
+    this.#createValueInput();
+    this.#createSearchInput();
+    this.#createOptions();
+
+    document.addEventListener("click", (e) => {
+      if (e.target !== this.#searchInput) {
+        this.classList.remove("active");
+      }
+    });
+  }
+
+  disconnectedCallback() {
+    this.#searchInput.removeEventListener("click", this.#showOptions.bind(this));
+    this.#searchInput.removeEventListener("focus", this.#showOptions.bind(this));
+    this.#searchInput.removeEventListener("input", this.#filterOptions.bind(this));
+  }
+}
+
+window.customElements.define("minimal-select", MinimalSelect);
